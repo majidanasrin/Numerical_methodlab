b014cb0f8deb6b3ad3034688d68e472ba764dcb7
majidanasrin <147602095+majidanasrin@users.noreply.github.com>
2025-04-25 12:24:58 +0530

    Add files via upload

---
+++ FDD.py
@@ -0,0 +1,13 @@
+import math
+
+def FDD(f,x,h):
+    return ((f(x+h)-f(x))/h)
+
+func = input("enter the funcation in terms of x (eg: x**3-2*x,math.sin(x)) : ")
+x0 = float(input("enetr the point at which to be determined : "))
+h = float(input("Enter the stepsize :  "))
+
+f = lambda x: eval(func)
+
+result = FDD(f,x0,h)
+print(f"FDD at x ={x0} is approximately {result}")
